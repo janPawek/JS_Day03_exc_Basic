12faee810a00c1cf04613b539b94ead83ef17931
Jan <jan.pawek@outlook.com>
2024-02-01 12:02:31 +0100

    add

--- script.js
+++ script.js
@@ -15,8 +15,7 @@ for (let val of numbers) {
 }
 console.log(max);
 
-// Aufgabe: ex 2 | Temperature v2.0
-// Now when you have program Temperature v1.0 done from the other day, it is time to upgrade it to the version 2.0. Now you should create a nice looking responsive front-end for your program (mobile friendly) and create more messages (e.g. if the temperature is above 32c output "The weather is hot" etc.) and you should display a proper image for the current weather condition.
+
 
 
 
